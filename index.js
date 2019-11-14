2a6c90a109c92edef68ea96055b2c26d676d38e9
Nabil Ridhwanshah <57290483+nabilridhwan@users.noreply.github.com>
2019-11-14 19:12:34 +0800

    (v2)
    * Recursive file view and nested directory is working!
    
    * Refactored
    
    * Beautify
    
    * Added a ROOT directory in the __dir_contents__
    
    * UPDATED README FOR V2

--- index.js
+++ index.js
@@ -3,62 +3,124 @@ let express = require('express');
 let fs = require('fs');
 let app = express();
 
+const port = 3030;
+
+// The IP and PORT is stored here
 let server_on = null;
 
+// Current directory contents
 let __dir_contents__ = [];
+let dir_log = ["/"]
+let count = 0;
+
+app.use((request, response, next) => {
+
+    // Count is increase because every directory that the user visits is pushed to dir_log, so if the user visited that page for the 39th time, so its current directory is dir_log[count]
+
+    // dir_log[count] is to get the current latest directory, the directory that is pushed is all including its parent(s)
+    count++
+
+    // For GET root
+    if (request.url == "/") {
+
+        // Push '/'
+        dir_log.push("/")
+    } else {
+
+        // Push the directory query (d)
+        dir_log.push(request.query.d)
+    }
+
+    console.log(dir_log)
+    next();
+})
 
 app.get('/', (request, response) => {
-    __getcontents__(__dirname)
-    response.send(__dir_contents__)
+    __getcontents__(__dirname, dir_log)
+    setTimeout(() => {
+        response.send(__dir_contents__)
+    }, 5);
 })
 
-app.get('/dir/:directory', (request,response)=>{
-    __getcontents__(__dirname + "/" + request.params.directory);
-    response.send(__dir_contents__);
+app.get('/dir/', (request, response) => {
+    __getcontents__(__dirname + "/" + request.query.d, dir_log);
+    setTimeout(() => {
+        response.send(__dir_contents__);
+    }, 5);
 })
 
-app.get('/file/:filename', (request, response) => {
-    response.sendFile(__dirname + "/" + request.params.filename)
+app.get('/file/', (request, response) => {
+    response.sendFile(__dirname + "/" + request.query.f)
 })
 
-// Run on private ip
+// Run on PRIVATE IP
 network.get_private_ip(function (err, ip) {
-    app.listen(3000, ip, () => {
-        server_on = `${ip}:3000`
+
+    // Listen
+    app.listen(port, ip, () => {
+        server_on = `${ip}:${port}`
         console.log(`Listening on: ${server_on}`)
     })
 })
 
 
-// Function to get contents of directory
-function __getcontents__(path){
+// Function to get contents of the directory
+function __getcontents__(path, input_dir_history) {
+
+    // Read the directory for both files and directories
     fs.readdir(path, (err, items) => {
 
-        // Empty
+        // Empty the dir_contents
         __dir_contents__ = [];
 
-        if(items.length !== 0){
-            for(let i = 0; i < items.length; i++){
-                fs.readdir(items[i], (err,inner_dir_test) => {
+        // If the items found inside is not empty ['file1', 'file2']
+        if (items.length !== 0) {
+
+            // Loop through the items
+            for (let i = 1; i < items.length; i++) {
+
+                // Check if the items inside are files or direcotyr
+                fs.readdir("./" + input_dir_history[count] + "/" + items[i], (err, inner_dir_test) => {
 
                     // Not a dir items return undefined!
-                    if(inner_dir_test == undefined){
+                    if (!inner_dir_test) {
+
+                        let filetype = items[i].split(".")[items[i].split(".").length - 1]
 
                         // NOT A DIR
                         __dir_contents__[i] = {
                             "name": items[i],
-                            "type": "file",
-                            "get_file_link": `http://${server_on}/file/${items[i]}`,
-                            "warning": "Recursive file viewing is not yet available!"
+                            "type": filetype,
+                            "link": {
+                                "dir": null,
+                                "file": `http://${server_on}/file/?f=${input_dir_history[count]}/${items[i]}`
+                            },
                         }
-                    }else{
+
+                        // as you see above, input_dir_history[count] is to get the current latest directory, the directory that is pushed is all including its parent(s)
+                    } else {
 
                         // IS A DIR
                         __dir_contents__[i] = {
                             "name": items[i],
                             "type": "dir",
-                            "get_dir_link": `http://${server_on}/dir/${items[i]}`
+                            "link": {
+                                "dir": `http://${server_on}/dir/?d=${input_dir_history[count]}/${items[i]}`,
+                                "file": null
+                            },
                         }
+
+                        // as you see above, input_dir_history[count] is to get the current latest directory, the directory that is pushed is all including its parent(s)
+
+                    }
+
+                    __dir_contents__[0] = {
+                        "name": "ROOT",
+                        "type": "dir",
+                        "link": {
+                            "dir": `http://${server_on}/`,
+                            "file": null
+                        },
                     }
                 })
             }
